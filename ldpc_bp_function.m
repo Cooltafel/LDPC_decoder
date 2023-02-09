0b11bf90605e274698e26a3e0db280d691543f31
Cooltafel <72130556+Cooltafel@users.noreply.github.com>
2023-02-09 09:34:29 +0100

    Update ldpc_bp_function.m
    Improved the decoder!

--- ldpc_bp_function.m
+++ ldpc_bp_function.m
@@ -2,58 +2,46 @@ function bits_hat = ldpc_bp_function(L, H, max_iter) %#codegen
 nb_c_nodes = size(H,1); % the number of check nodes (= number of rows)
 nb_v_nodes = size(H,2); % the number of v_nodes (= number of columns)
 
-for idx_iter_llr = 1:1:max_iter
-                % 1) Initial LLRs from VNs to CNs
-                if(idx_iter_llr == 1)
-                    M = H .* L';
-                end
-                
+% 1) Initial LLRs from VNs to CNs
+M = H .* L';
+L_int = L;
+
+for idx_iter_llr = 1:1:max_iter             
                 % 2) LLRs from CNs to VNs
                 E = zeros(nb_c_nodes, nb_v_nodes);
                 for idx_row = 1:1:nb_c_nodes
+                    h_tmp = logical(H(idx_row,:));
                     m_tmp = M(idx_row, :);
-                    [~, col_tmp] = find(H(idx_row, :));
-                    m_tmp = m_tmp(:,col_tmp);
-                    m_tmp(m_tmp < -4) = -4;
-                    m_tmp(m_tmp > 4) = 4;
+                    m_tmp = m_tmp(:, h_tmp);
                     matrix = eye(length(m_tmp)) < 1;
-                    matrix_tmp = matrix .* m_tmp;
-                    m_tmp_1 = tanh(matrix_tmp/2);
-%                     m_tmp_2 = atanh(prod(m_tmp_1 + eye(length(m_tmp)), 2));
-                    x = prod(m_tmp_1 + eye(length(m_tmp)), 2);
-                    m_tmp_2 = log((1 + x)./(1 - x));
-                    E(idx_row, col_tmp) = 2 * m_tmp_2;
-%                     for idx_col = 1:1:length(col_tmp)
-%                         tmp = m_tmp;
-%                         tmp(:,idx_col) = 1;
-%                         res = atanh(prod(tmp));
-%                         res(res < -19.07) = -19.07;
-%                         res(res > 19.07) = 19.07;
-%                         E(idx_row, col_tmp(idx_col)) = 2*res;
-%                     end
+                    matrix_tmp = matrix .* tanh(m_tmp/2);
+%                     m_tmp_1 = matrix_tmp;
+%                     m_tmp_2 = 2*atanh(prod(m_tmp_1 + eye(length(m_tmp)), 2));
+                    x = prod(matrix_tmp + eye(length(m_tmp)), 2);
+%                     m_tmp_2 = log((1 + x)./(1 - x));
+                    E(idx_row, h_tmp) = log((1 + x)./(1 - x));
+%                     E(idx_row, h_tmp) = 2*atanh(x);
                 end
 
                 % 3) LLRs from VNs to CNs
                 M = zeros(nb_c_nodes, nb_v_nodes);
                 for idx_col = 1:1:nb_v_nodes
-                    e_tmp = E(:, idx_col);
-                    [row_tmp, ~] = find(H(:, idx_col));
-                    e_tmp = e_tmp(row_tmp, :);
+                    h_tmp = logical(H(:, idx_col));
+                    e_tmp = E(h_tmp, idx_col);
+%                     e_tmp = e_tmp(h_tmp, :);
                     if(length(e_tmp) ~= 1)
                         matrix = eye(length(e_tmp)) < 1;
                         matrix_tmp = matrix .* e_tmp;
                         e_tmp_2 = sum(matrix_tmp, 1);
-%                         e_tmp_2(e_tmp_2 < -19.07) = -19.07;
-%                         e_tmp_2(e_tmp_2 > 19.07) = 19.07;
-                        M(row_tmp, idx_col) = e_tmp_2;
+                        M(h_tmp, idx_col) = e_tmp_2 + L_int(idx_col) ;
                     else 
-                        M(row_tmp, idx_col) = e_tmp;
+                        M(h_tmp, idx_col) = e_tmp + L_int(idx_col);
                     end
                 end
 
                 % Update total LLRs
                 E_tmp = sum(E, 1);
-                L = L + E_tmp';
+                L = L_int + E_tmp';
                 
                 % Check if a correct codeword is found 
                 bits_hat = L < 0;
